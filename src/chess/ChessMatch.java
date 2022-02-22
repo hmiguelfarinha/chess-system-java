dc8d5860b0a8ee67bb69c0930d352beab596315a
Hugo Farinha <hugo.m.farinha@gmail.com>
2022-02-22 12:20:26 +0000

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -24,9 +23,12 @@ public class ChessMatch { //coração do sistema de xadrez
 	return mat;	
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) { //método para informar o initialSteup das posição das peças no sistema do xadrez em vez do sistema da matrix
+		board.placePiece(piece, new ChessPosition(column, row).toPosition()); // .toPosition() é para converter para a posição de matrix
+	}
 	private void initialSetup() { //método responsavel por iniciar a partida colocando as peças no tabuleiro
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,39 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+	private char column;
+	private int row;
+
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) { //programação defensiva
+			throw new chessException("Error instantiating ChessPostion. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	protected Position toPosition() { //vai converter a chessposition para a posição normal Logia -> matrix_row = 8 - chess_row e matrix_column = chess_column - unicode 'a'
+		return new Position(8 - row, column - 'a');
+	}
+	
+	protected static ChessPosition fromPosition(Position position) { //vai converter de posição normal para chesspostion 
+		return new ChessPosition((char)('a'- position.getColumn()), 8 - position.getRow()); //é preciso fazer o casting porque não converte automaticamente para char
+	}
+	
+	@Override
+	
+	public String toString() {
+		return "" + column + row; // as "" é para forçar o compilador a perceber que é uma concatenação de string, sem as aspas dá erro
+	}
+}
