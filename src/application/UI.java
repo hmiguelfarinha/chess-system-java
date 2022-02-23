54ae1caf9e1b068a931b9452d82fe174b69cdd1e
Hugo Farinha <hugo.m.farinha@gmail.com>
2022-02-23 15:36:01 +0000

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,29 +1,56 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
 
-	public static void printBoard(ChessPiece[][] pieces) { //método para imprimir o tabuleiro, é estático! recebe o chesspiece chamando pieces
-		for (int i = 0; i<pieces.length; i++) {
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	public static void printBoard(ChessPiece[][] pieces) { // método para imprimir o tabuleiro, é estático! recebe o
+															// chesspiece chamando pieces
+		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " "); // imprime o 8 , 7 , 6 por aí fora da primeira coluna
-			for (int j = 0; j<pieces.length; j++) { // j<pieces.length considera-se que a matriz é quadrada então pode ser assim
-				printPiece(pieces[i][j]); //imprime a peça
+			for (int j = 0; j < pieces.length; j++) { // j<pieces.length considera-se que a matriz é quadrada então pode
+														// ser assim
+				printPiece(pieces[i][j]); // imprime a peça
 			}
-			System.out.println(); //saltar de linha
+			System.out.println(); // saltar de linha
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
-	private static void printPiece(ChessPiece piece) { //método auxiliar para imprimir uma peça
-		if (piece == null) { //verificação se existe peça na posição do tabuleiro
-			System.out.print("-"); //imprime como se fosse vazio
+
+	private static void printPiece(ChessPiece piece) {
+		if (piece == null) {
+			System.out.print("-");
 		}
-		else {
-			System.out.print(piece); // se existe peça então imprime
-		}
-		System.out.print(" "); //impressão de um espaço em branco para as peças não ficarem demasido juntas
+        else {
+            if (piece.getColor() == Color.WHITE) { //verificar se a peça a imprimir é preta ou branca, a preta está a ser imprimida a amarelo 
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
 	}
-	
-	
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,7 +9,7 @@ public class ChessPosition {
 
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) { //programação defensiva
-			throw new chessException("Error instantiating ChessPostion. Valid values are from a1 to h8.");
+			throw new ChessException("Error instantiating ChessPostion. Valid values are from a1 to h8.");
 		}
 		this.column = column;
 		this.row = row;
